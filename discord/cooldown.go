a3fe8e9263970890e0e1cedacc23c3ebc17f8987
Warstrolo <warstrolo.wizard@gmail.com>
2021-06-23 08:36:12 +0200

    Added cooldown func for Hcooldown

--- discord/cooldown.go
+++ discord/cooldown.go
@@ -58,14 +58,63 @@ func LCooldownGet(user structure.Member, command string, exp time.Duration) (boo
 	
 } 
 
-func HCooldownGet(user structure.Member, cmd string) bool{
-	return true 
+func HCooldownGet(user structure.Member, command string, exp time.Duration) (bool, error){
+	valid, res := BotVar.GetCache().GetLcooldown(user.ID, command)
+	if valid {
+		t := res.Add(exp) 
+		//println(t, "  ", time.Now().Unix())
+		if ( t.UTC().Unix() > time.Now().UTC().Unix() ) {
+			return true, nil
+		}
+		return false, nil
+	}
+	if config.DBenabled && config.LCooldownDB {
+		resdb, err := rethinkdb.DB(config.DBdatabase).Table(config.DBcooldowntable).Get(user.ID).Run(database.DBsession)
+		if err != nil {
+			return false, errors.New("DataBase Error")
+		}
+		var doc map[string]string
+		resdb.Next(&doc)
+		value, exist := doc["data."+command]
+		if !exist {
+			return false, nil
+		}
+		t, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return false, errors.New("Error in string conv")
+		}
+		timeparsed := time.Unix(t, 0)
+		if ( timeparsed.Add(exp).Unix()  > time.Now().Unix() ) {
+			BotVar.GetCache().SetLcool(user, command, timeparsed)
+			return true, nil
+		}
+		return false, nil
+
+	}
+	return false, nil
 } 
 
 func LCooldownset(user structure.Member, command string){
 	t := time.Now()
 	BotVar.GetCache().SetLcool(user, command, t)
 	if config.DBenabled && config.LCooldownDB {
+		
+		err := rethinkdb.DB(config.DBdatabase).Table(config.DBcooldowntable).Insert(map[string]string{
+			"id": user.ID,
+			"data."+command: strconv.FormatInt(t.UTC().Unix(), 10),
+		}, rethinkdb.InsertOpts{
+			Conflict: "update",
+		}).Exec(database.DBsession)
+		if err != nil {
+			fmt.Println(cmd.NewFlag(1),"Error occured when writing cooldown to DB")
+		}
+
+	}
+}
+func HCooldownset(user structure.Member, command string){
+	t := time.Now()
+	BotVar.GetCache().SetHcool(user, command, t)
+	if config.DBenabled && config.HCooldownDB {
 		
 		err := rethinkdb.DB(config.DBdatabase).Table(config.DBcooldowntable).Insert(map[string]string{
 			"id": user.ID,
